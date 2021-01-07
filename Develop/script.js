7041831f9c77646e4223e7a5b11e827b74489ab0
Ashton Pierce <coalpierce@gmail.com>
2021-01-06 21:59:48 -0600

    initial push

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+// Assignment code here
+var txt ="A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
